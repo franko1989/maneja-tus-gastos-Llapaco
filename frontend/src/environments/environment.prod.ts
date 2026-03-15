93a882182bb1cb1aae4f07b71ec7b905731aaa7f
franko1989 <carinzaghi8@gmail.com>
2026-03-15 00:36:51 -0400

    changes 2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com
+  //abcsd
   apiUrl: 'https://maneja-tus-gastos-api-ppv5.onrender.com',
 };
 
